713ff4eff1ecdd54a17b4215854ce4ed7654abf2
Yoga Setiawan <mryoga1995@gmail.com>
2019-06-16 09:12:11 +0700

    Optimalisasi soal no 6

--- src/soal-no6.js
+++ src/soal-no6.js
@@ -14,12 +14,43 @@
 
  **/
 
-let result = 67;
+let biner = (result) => {
+    const angka = result;
+    let bil = [];
+    for (let i = 0; result > 0; i++) {
+        bil.push(result % 2);
+        result = parseInt(result / 2);
+    }
 
-let bil = [];
-for (let i = 0; result > 0; i++) {
-    bil.push(result % 2);
-    result = parseInt(result / 2);
+    bil.reverse();
 
-}
-console.log(`Angka Biner : ${bil.reverse().join("")}`)
+    switch (bil.length % 4) {
+        case 3 :
+            bil.splice(0, 0, 0);
+            break;
+        case 2 :
+            bil.splice(0, 0, 0);
+            bil.splice(0, 0, 0);
+            break;
+        case 1 :
+            bil.splice(0, 0, 0);
+            bil.splice(0, 0, 0);
+            bil.splice(0, 0, 0);
+            break;
+    }
+
+    for (let k = 0; k < bil.length; k++) {
+        if (k % 5 == 0) {
+            bil.splice(k, 0, " ");
+        }
+    }
+
+    console.log(`Angka Biner dari ${angka} adalah: ${bil.join("")}`);
+};
+
+biner(150)
+
+
+
+
+
